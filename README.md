4c0cfffc47286e4733b19d2a1683888b58a85913
alombi <alessandrolombardo@AirdiAlessandro.home>
2021-01-24 18:46:30 +0100

    Added /homepage

--- README.md
+++ README.md
@@ -1,15 +1,31 @@
 # Unofficial RoutineHub API
 This project is a serverless API hosted on Vercel that retrieves some useful informations that aren't available from the official API (yet) about Shortcuts hosted on RoutineHub.
 ## Documentation
+* [Homepage](https://github.com/alombi/rh-api/blob/master/README.md#homepage-get)
 * [Changelog](https://github.com/alombi/rh-api/blob/master/README.md#changelog-get)
-* [Stats](https://github.com/alombi/rh-api/blob/master/README.md#shortcut-get)
+* [Shortcut](https://github.com/alombi/rh-api/blob/master/README.md#shortcut-get)
 * [Author](https://github.com/alombi/rh-api/blob/master/README.md#author-get)
 
+
+## Homepage (GET)
+Heads over to https://rh-api.alombi.xyz/homepage
+#### Parameters
+* `trending` = an array containing 6 elements
+* `new` = an array containing 6 elements
+* `recently-updated` = an array containing 6 elements
+
+Each array has the same elements' structure:
+* `name` = the name of the shortcut
+* `id` = the RoutineHub ID of the shortcuts
+* `description` = the shortcut's brief description
+* `downloads` = the shortcut's downloads count
+* `hearts` = the shortcut's hearts count
+* `link` = the link to the shortcut's RoutineHub page
+* `api_link` = the link to the `/shortcut` endpoint
 
 ## Changelog (GET)
 Heads over to https://rh-api.alombi.xyz/changelog and add a parameter to this URL, containing your shortcut's RH ID. For example https://rh-api.alombi.xyz/changelog?id=1
 #### Parameters
-* `id` = the RoutineHub ID of the shortcuts
 * `name` = the name of the shortcut
 * `updates` = the number of updates the author made
 * `versions` = an array that contains as many elements as the `updates` value. Each element has these parameters:
---
+++ api/homepage.py
@@ -0,0 +1,62 @@
+import bs4, requests
+from http.server import BaseHTTPRequestHandler
+
+def scrapeText():
+   url = f'https://routinehub.co'
+   req = requests.get(url)
+   req.raise_for_status()
+   soup = bs4.BeautifulSoup(req.text, 'html.parser')
+   return soup
+
+def extract(soup, selector):
+  elems = soup.select(selector)
+  return elems
+
+
+class handler(BaseHTTPRequestHandler):
+   def do_GET(self):
+      soup = scrapeText()
+      trending = {
+         'trending':[],
+         'new':[],
+         'recently-updated':[]
+      }
+      i = 1
+      j = 1
+      while j < 4:
+         i = 1
+         if j == 1:
+            param = 'trending'
+         elif j == 2:
+            param = 'new'
+         elif j == 3:
+            param = 'recently-updated'
+         while i < 7:
+            elem = extract(soup, f'#shortcut-lists > div:nth-child(2) > div.{param}.home-list > div > div:nth-child({i})')
+            name = str(elem[0].select('a > div > div > div > div > div > p > strong')).replace('[<strong>', '').replace('</strong>]', '')
+            print(name)
+            desc = str(elem[0].select(f'#shortcut-lists > div:nth-child(2) > div.{param}.home-list > div > div:nth-child({i}) > a > div > div > div > div > div > p > small')).replace('[<small>', '').replace('</small>]', '')
+            downloads = int(elem[0].select(f'#shortcut-lists > div:nth-child(2) > div.{param}.home-list > div > div:nth-child({i}) > a > div > div > div > div > nav > div.level-right > span:nth-child(1) > small')[0].get_text())
+            hearts = int(elem[0].select(f'#shortcut-lists > div:nth-child(2) > div.{param}.home-list > div > div:nth-child({i}) > a > div > div > div > div > nav > div.level-right > span:nth-child(2) > small')[0].get_text())
+            link = 'https://routinehub.co' + elem[0].select(f'#shortcut-lists > div:nth-child(2) > div.{param}.home-list > div > div:nth-child({i}) > a')[0]['href']
+            RHid = str(elem[0].select(f'#shortcut-lists > div:nth-child(2) > div.{param}.home-list > div > div:nth-child({i}) > a')[0]['href']).replace('/shortcut/', '').replace('/', '')
+            api_link = 'https://rh-api.alombi.xyz/shortcut?id=' + RHid
+            shortcut = {
+               'name':name,
+               'id':RHid,
+               'description':desc,
+               'downloads':downloads,
+               'hearts':hearts,
+               'link':link,
+               'api_link':api_link
+            }
+            i = i + 1
+            trending[param].append(shortcut)
+         j = j + 1      
+
+      data = str(trending).replace('\'', '\"')
+      self.send_response(200)
+      self.send_header('Content-type', 'text/plain')
+      self.end_headers()
+      self.wfile.write(data.encode())
+      return
