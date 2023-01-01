7d5c7e912cb4b9b318c1ff118abe7fa917afb956
alombi <alombi@hotmail.com>
2023-01-01 11:04:02 +0100

    Changes packages

--- src/routes/changelog/+server.ts
+++ src/routes/changelog/+server.ts
@@ -1,12 +1,21 @@
 import { json } from '@sveltejs/kit';
-import puppeteer from 'puppeteer';
+import puppeteer from 'puppeteer-core';
+import edgeChromium from 'chrome-aws-lambda'
+const LOCAL_CHROME_EXECUTABLE = '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome'
 
 export async function GET({url, setHeaders}){
     const id = Number(url.searchParams.get('id'));
     const baseURL: string = `https://routinehub.co/shortcut/${id}/changelog`;
     if(id == null || id == 0) return json({error: 'No ID provided.'});
 
-    const browser = await puppeteer.launch();
+    const executablePath = await edgeChromium.executablePath || LOCAL_CHROME_EXECUTABLE
+  
+    const browser = await puppeteer.launch({
+        executablePath,
+        args: edgeChromium.args,
+        headless: false,
+    })
+    //const browser = await puppeteer.launch();
     const page = await browser.newPage();
     page.setUserAgent('Mozilla/5.0 (iPhone; U; CPU iPhone OS 3_0 like Mac OS X; en-us) AppleWebKit/528.18 (KHTML, like Gecko) Version/4.0 Mobile/7A341 Safari/528.16')
     //page.setUserAgent('DuckDuckBot')
