4bb920ebd066358fe9b1d1645db19f3a021b4e65
alombi <alombi@hotmail.com>
2023-01-04 13:52:28 +0100

    Switches to puppeteer-core and chromium

--- api/changelog.ts
+++ api/changelog.ts
@@ -1,4 +1,6 @@
-import puppeteer from 'puppeteer';
+import puppeteer from 'puppeteer-core';
+import edgeChromium from 'chrome-aws-lambda';
+const LOCAL_CHROME_EXECUTABLE = '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome';
 
 async function checkForResults(page){
     let notFoundString = await page.waitForSelector('#content > div > div > div');
@@ -14,7 +16,12 @@ module.exports = async (req, res) => {
     const query: string = req.query.q;
     const baseURL: string = `https://routinehub.co/search/?q=${query}`;
 
-    const browser = await puppeteer.launch();
+    const executablePath = await edgeChromium.executablePath || LOCAL_CHROME_EXECUTABLE
+    const browser = await puppeteer.launch({
+        executablePath,
+        args: edgeChromium.args,
+        headless: false,
+    })
     const page = await browser.newPage();
     page.setUserAgent('Mozilla/5.0 (iPhone; U; CPU iPhone OS 3_0 like Mac OS X; en-us) AppleWebKit/528.18 (KHTML, like Gecko) Version/4.0 Mobile/7A341 Safari/528.16')
     await page.goto(baseURL);
